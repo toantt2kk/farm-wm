d4bf7b464126d11ae90c4faea5dfcae32efa6f97
toantt2kk <toantt2000k@gmail.com>
2025-02-22 16:29:20 +0700

    add new script get size macos

--- src/utils/command.js
+++ src/utils/command.js
@@ -1,12 +1,31 @@
 import { execSync } from "child_process";
+import os from "os";
 
 const getScreenSize = () => {
-  const output = execSync(
-    "wmic path Win32_VideoController get CurrentHorizontalResolution,CurrentVerticalResolution"
-  ).toString();
-  const match = output.match(/(\\d+)\\s+(\\d+)/);
-  return match
-    ? { width: parseInt(match[1]), height: parseInt(match[2]) }
-    : { width: 1920, height: 1080 };
+  try {
+    if (os.platform() === "win32") {
+      const output = execSync(
+        "wmic path Win32_VideoController get CurrentHorizontalResolution,CurrentVerticalResolution"
+      ).toString();
+      const match = output.match(/(\d+)\s+(\d+)/);
+      return match
+        ? { width: parseInt(match[1]), height: parseInt(match[2]) }
+        : { width: 1920, height: 1080 };
+    } else if (os.platform() === "darwin") {
+      const output = execSync(
+        "system_profiler SPDisplaysDataType | awk '/Resolution/{print $2, $4}'"
+      )
+        .toString()
+        .trim();
+      const match = output.match(/(\d+)\s+(\d+)/);
+      return match
+        ? { width: parseInt(match[1]), height: parseInt(match[2]) }
+        : { width: 1920, height: 1080 };
+    }
+  } catch (error) {
+    console.error("Error fetching screen resolution:", error);
+  }
+  return { width: 1920, height: 1080 }; // Default fallback
 };
+
 export { getScreenSize };
